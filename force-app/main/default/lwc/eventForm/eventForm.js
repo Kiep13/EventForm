e283c93e174040edfbbcc8ebb9c1714d94de2d8d
nina_smirnova <kiep13@bk.ru>
2020-11-24 12:53:43 +0300

    added ability of updating and creating records

--- force-app/main/default/lwc/eventForm/eventForm.js
+++ force-app/main/default/lwc/eventForm/eventForm.js
@@ -1,13 +1,27 @@
 import { LightningElement, api, track } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 import getFieldsInfo from '@salesforce/apex/EventController.getFieldsInfo';
+import upsertEvent from '@salesforce/apex/EventController.upsertEvent';
+
+import ID_FIELD from '@salesforce/schema/Event.Id';
 
 export default class EventForm extends LightningElement {
+
+  SUCCESS_TITLE = 'Success';
+  ERROR_CREATE_TITLE = 'Error creating record';
+  ERROR_UPDATE_TITLE = 'Error updating record';
+  ERROR_GET_FIELDS = 'Error while getting fields info';
+
+  SUCCESS_CREATE_MESSAGE = 'Event created';
+  SUCCESS_UPDATE_MESSAGE = 'Event updated';
+
+  SUCCESS_VARIANT = 'success';
+  ERROR_VARIANT = 'error';
 
   @api recordId;
 
   @track fields = [];
-  @track event;
 
   @track error;
 
@@ -18,17 +32,13 @@ export default class EventForm extends LightningElement {
   loadFields() {
     getFieldsInfo({recordId: this.recordId})
       .then((result) => {
-        console.log(result);
-        //result = this.glueResult(result);
-        //console.log(result);
         this.fields = result;
         this.glueResult();
-        
-        console.log(this.fields);
       })
       .catch(error => {
-        //this.error = error;
-        console.log(error);
+        this.showToastEvent(this.ERROR_GET_FIELDS,
+                            error.body.message,
+                            this.ERROR_VARIANT);
       });
   }
 
@@ -47,5 +57,72 @@ export default class EventForm extends LightningElement {
       case 'BOOLEAN': return 'checkbox';
       default: return datatype;
     }
+  }
+
+  submit() {
+    const inputs = [...this.template.querySelectorAll('lightning-input')];
+    const allValid = inputs.reduce((validSoFar, inputFields) => {
+                        inputFields.reportValidity();
+                        return validSoFar && inputFields.checkValidity();
+                      }, true);
+
+    if(!allValid) {
+      return;
+    }
+
+    let record = {};
+
+    inputs.forEach((input, index) => {
+      //console.log(this.fields[index].apiName, input.value, input.checked)
+
+      let value;
+
+      switch(this.fields[index].dataType) {
+        case 'checkbox': {
+          value = input.checked;
+        } break;
+        case 'number': {
+          value = +input.value;
+        } break;
+        default: {
+          value = input.value;
+        }
+      }
+
+      this.fields[index].value = value;
+      record[this.fields[index].apiName] = value;
+    });
+    
+    if(this.recordId != null) record[ID_FIELD.fieldApiName] = this.recordId;
+
+    this.makeUpsertRequest(record);
+  }
+
+  makeUpsertRequest(record) {
+
+    console.log(5, record);
+
+    upsertEvent({event : record})
+      .then(() => {
+        this.showToastEvent(this.SUCCESS_TITLE,
+                            this.SUCCESS_UPDATE_MESSAGE,
+                            this.SUCCESS_VARIANT)
+      })
+      .catch(error => {
+        console.log(error);
+        this.showToastEvent(this.ERROR_UPDATE_TITLE, 
+                            error.body.message,
+                            this.ERROR_VARIANT);
+      });
+  }
+
+  showToastEvent(title, message, variant) {
+    this.dispatchEvent(
+      new ShowToastEvent({
+        title,
+        message,
+        variant
+      })
+    );
   }
 }
